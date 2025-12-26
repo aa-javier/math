a62101206c8c45ada8a67829a7efb58871dcb5b6
AA Javier <arkhaniphone@gmail.com>
2025-12-26 22:00:01 +0700

    Create JaringJaring.jsx

---
+++ src/pages/JaringJaring.jsx
@@ -0,0 +1,9 @@
+export default function JaringJaring({ onBack }) {
+  return (
+    <div style={{ padding: 20 }}>
+      <button onClick={onBack}>← Kembali</button>
+      <h2>Jaring-jaring Kubus</h2>
+      <p>Di sini nanti akan ada gambar dan animasi jaring-jaring.</p>
+    </div>
+  )
+}
